b25bddbb601c1f381a3ae76601f5812014c51218
Louisa <louisa.encelf1230@gmail.com>
2024-02-16 17:46:19 +0800

    new txt file

--- IPT2_LabQuiz2/src/ipt2_labquiz2/IPT2_LabQuiz2.java
+++ IPT2_LabQuiz2/src/ipt2_labquiz2/IPT2_LabQuiz2.java
@@ -5,11 +5,15 @@ package ipt2_labquiz2;
 public class IPT2_LabQuiz2 {
 
     // Addition Method
+    public static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
     
     //Subtraction Method
     
     
     //Multiplication Method
+    
     
     //Division Method
     
