1b44b43bc72b083c4d17f244e9006751d201a2e3
Louisa Encel Flores <louisa.encelf1230@gmail.com>
2024-02-16 23:50:13 +0800

    added the functionality of it

--- IPT2Quiz2.java
+++ IPT2Quiz2.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class IPT2Quiz2 {
     
     
@@ -27,8 +30,42 @@ public class IPT2Quiz2 {
         return num1 - num2;
     }
     
+     
+     
     public static void main(String[] args) {
-        // Main program logic will go here
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2;
+        char operation;
+
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextDouble();
+        
+        System.out.print("Enter operation (+, -, *, /): ");
+        operation = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = addition(num1, num2);
+                break;
+            case '-':
+                result = subtraction(num1, num2);
+                break;
+            case '*':
+                result = multiplication(num1, num2);
+                break;
+            case '/':
+                result = division(num1, num2);
+                break;
+            default:
+                System.out.println("Invalid operation!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
     }
 }
-
