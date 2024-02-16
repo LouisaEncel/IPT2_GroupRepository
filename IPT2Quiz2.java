e977fce80ffd61812945b7f4e9c132a84dcaaef9
GraceAnne <Cutie>
2024-02-16 22:57:27 +0800

    Added the multiplication method

--- IPT2Quiz2.java
+++ IPT2Quiz2.java
@@ -6,6 +6,12 @@ public class IPT2Quiz2 {
         return num1 + num2;
     }
     
+    //Multiplication Method
+    public static double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+   
+    
     public static void main(String[] args) {
         // Main program logic will go here
     }
