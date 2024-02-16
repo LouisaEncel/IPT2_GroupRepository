3f1ef427363ef2f0ceedc59b8494ca9b1a5dbfac
unknown <joannecarlatabago@gmail.com>
2024-02-16 23:16:56 +0800

    Eh sorry mali branch

--- IPT2Quiz2.java
+++ IPT2Quiz2.java
@@ -10,17 +10,7 @@ public class IPT2Quiz2 {
     public static double multiplication(double num1, double num2) {
         return num1 * num2;
     }
-  
-    
-    //Division Method
-    public static double division(double num1, double num2) {
-        if (num2 != 0) {
-            return num1 / num2;
-        } else {
-            System.out.println("Error: Division by zero!");
-            return Double.NaN; // NaN (Not a Number) represents an undefined result
-        }
-    }
+   
     
     public static void main(String[] args) {
         // Main program logic will go here
