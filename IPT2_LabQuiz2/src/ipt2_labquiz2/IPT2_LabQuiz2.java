b0032d8195afedf6c0927c4f9fd0eaf1d1ffadee
Louisa <louisa.encelf1230@gmail.com>
2024-02-16 17:32:56 +0800

    created anew file for calculator

---
+++ IPT2_LabQuiz2/src/ipt2_labquiz2/IPT2_LabQuiz2.java
@@ -0,0 +1,21 @@
+
+package ipt2_labquiz2;
+
+
+public class IPT2_LabQuiz2 {
+
+    // Addition Method
+    
+    //Subtraction Method
+    
+    
+    //Multiplication Method
+    
+    //Division Method
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
